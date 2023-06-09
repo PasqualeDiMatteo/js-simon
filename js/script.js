114c952b4921b69dbcae37ed775e38d1b609afe7
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-09 17:33:58 +0200

    [JS] Added Logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
 console.log("JSOK");
+
+// Recupero gli elementi dal DOM
+
+const countdownElement = document.getElementById("countdown");
+const numberElement = document.getElementById("number");
+const cardElement = document.getElementById("card");
+const message = document.getElementById("message");
+
+// Dichiarazioni
+const numbersToGenerate = 5;
+let seconds = 5;
+countdownElement.innerText = seconds;
+let numberUser = [];
+let numbers = [];
+let score = 0;
+// Generiamo i numeri random
+
+function randomNumber() {
+  for (i = 0; i < numbersToGenerate; i++) {
+    do {
+      numbersRandom = Math.floor(Math.random() * 100) + 1;
+    } while (!numbers[i] === numbersRandom);
+    numbers.push(numbersRandom);
+    numberElement.innerHTML += ` <div class="mx-5">${numbers[i]}</div>`;
+  }
+
+  return numbers;
+}
+console.log(randomNumber());
+
+// Creaiamo il Countdown
+
+let countdown = setInterval(() => {
+  countdownElement.innerText = --seconds;
+  if (seconds === 0) {
+    clearInterval(countdown);
+    cardElement.classList.add("d-none");
+    setTimeout(function () {
+      for (i = 0; i < numbersToGenerate; i++) {
+        let valueUser = parseInt(prompt("Dammi i numeri"));
+        numberUser.push(valueUser);
+        if (numberUser.includes(numbers[i])) {
+          score++;
+        }
+      }
+      console.log(numberUser);
+      message.innerHTML = `Hai totalizzato ${score} punti!`;
+    }, 1000);
+  }
+}, 1000);
